9ad9b2eee29a2303ad3a57cd444af185dd955f1b
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-07 07:30:13 -0300

    Hooks Option API

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Header Aqui</h1>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Header montado...");
+    },
+
+    unmounted() {
+        console.log("Header desmontado...");
+    }
+}
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,37 @@
 <template>
   <main>
+    <Header />
     <h1>Home Page</h1>
+    <button v-on:click="count++">Count: {{ count }}</button>
   </main>
 </template>
+
+<script>
+import Header from '@/components/Header.vue';
+export default {
+  components: {
+    Header
+  },
+
+  data() {
+    return {
+      count: 1
+    }
+  },
+
+  methods: {
+    increment() {
+      console.log("Incrementando...");
+      this.count++
+    }
+  },
+  
+  mounted() {
+    console.log("Montado...");
+  },
+
+  updated() {
+    console.log("Atualizando...")
+  }
+}
+</script>
