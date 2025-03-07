2ced3a277687f88ea78d0cada9a5f74c271b15f5
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-07 07:38:16 -0300

    Aula composition API

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <template>
   <main>
     <h1>Home Page</h1>
+    <h3>Contador: {{ count }}</h3>
+    <button v-on:click="count++">Adicionar</button>
   </main>
 </template>
+
+<script setup>
+import { onMounted, onUpdated, ref } from 'vue';
+
+const count = ref(7)
+
+onMounted(() => {
+  console.log('Home page montada');
+}),
+
+onUpdated(() => {
+  console.log('Atualizando renderização');
+})
+
+</script>
