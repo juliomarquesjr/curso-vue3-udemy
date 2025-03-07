2e26eab40081f61e7c365bc4beeeee27727435a2
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-07 15:50:35 -0300

    Ajuste na API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,12 @@
-<?php
 
+<?php
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/users', function () {
     return User::all();
+});
+
+Route::get('/users/{id}', function ($id) {
+    return User::find($id);
 });
