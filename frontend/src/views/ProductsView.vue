709cc7288b8183269e2d0dbf561f1e4a5faebb63
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-08 19:45:25 -0300

    Get dados composition API

--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -2,39 +2,27 @@
     <div>
         <h1>Produtos Page</h1>
         <ul>
-            <li v-for="user in users" :key="user.id">
+            <li v-for="user in users.data" :key="user.id">
                 {{ user.first_name }} {{ user.last_name }} - Email: {{ user.email }}
             </li>
         </ul>
     </div>
 </template>
 
-<script>
+<script setup>
 import http from "@/services/http.js";
+import { onMounted, reactive } from "vue";
 
-export default {
-    name: "ProductsView",
+let users = reactive({data: []});
 
-    data() {
-        return {
-            users: [],
-        };
-    },
+onMounted(async () => {
+    try{
+        const {data} = await http.get("api/users");
+        users.data = data;
+    }
+    catch(error){
+        console.error(error);
+    }
+});
 
-    async mounted() {
-        try {
-            this.users = await this.getUsers();
-        } catch (error) {
-            console.log(error);
-        }
-    },
-
-    methods: {
-        async getUsers() {
-            const { data } = await http.get("/api/users");
-            return data;
-        }
-    },
-
-};
 </script>
