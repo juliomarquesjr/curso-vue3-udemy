ddd6d80f56b20ab9f7530326103e544f316dfd6a
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-09 19:33:14 -0300

    Adicionado Formulario

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,31 @@
+<template>
+
+    <header>
+      <h2 id="my-app">APP</h2>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <router-link to="/about">About</router-link>
+          <RouterLink to="/products">Produtos</RouterLink>
+          <RouterLink to="/form">Formulario</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+
+</template>
+
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
-<template>
-  <header>
-    <h2 id="my-app">APP</h2>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <router-link to="/about">About</router-link>
-        <RouterLink to="/products">Produtos</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
-
 <style>
-@import "@/assets/app.css";
+nav {
+  display: flex;
+  justify-content: space-around;
+  background-color: #f1f1f1;
+  padding: 10px;
+}
 </style>
---
+++ frontend/src/views/FormView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Formularios</h2>
+</template>
