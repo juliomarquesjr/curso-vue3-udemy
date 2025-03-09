28fc90de87d47ec3083714f2811d9cb3c619480f
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-09 09:50:57 -0300

    Condicional e V-bind

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,13 @@
 <template>
     <main>
       <h1>Home Page</h1>
+              <!-- <img :src="image" alt="Imagem" /> -->
+              <img v-bind:src="image" alt="Imagem" />
     </main>
   </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const image = ref("https://picsum.photos/300/300");
+</script>
--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
+
+        <Header v-if="showHeader" />
+
         <h1>Produtos Page</h1>
         <ul>
             <li v-for="user in users.data" :key="user.id">
-                {{ user.first_name }} {{ user.last_name }} - Email: {{ user.email }}
+                {{ user.first_name }} {{ user.last_name }} - Email: {{ user.email }} 
+                <span v-if="user.is_admin === 1">Administrador</span>
+                <span v-else>Usuario Comum</span>
             </li>
         </ul>
     </div>
@@ -11,9 +16,11 @@
 
 <script setup>
 import http from "@/services/http.js";
+import Header from "@/components/Header.vue";
 import { onMounted, reactive } from "vue";
 
 let users = reactive({data: []});
+const showHeader = true;
 
 onMounted(async () => {
     try{
