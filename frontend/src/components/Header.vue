37f73060396f23cdc9cd7c348e7076726f661497
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-09 09:47:02 -0300

    v-bind

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         <h1>Header Aqui</h1>
-        <h2>Componente usado para testes</h2>
+        <!-- <img :src="image" alt="Imagem" /> -->
+        <img v-bind:src="image" alt="Imagem" />
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            image: "https://picsum.photos/300/300",
+        };
+    },
+
     mounted() {
         console.log("Header montado...");
     },
