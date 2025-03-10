2e00387ff09afc2ee137ab9d156d96e3c939514c
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-09 21:49:51 -0300

    Formulario Option API

--- frontend/src/views/FormView.vue
+++ frontend/src/views/FormView.vue
@@ -1,9 +1,46 @@
 <template>
+  <div>
     <h2>Formularios</h2>
+
+    <p>
+      <input type="text" id="input1" v-model="userName" />
+    </p>
+
+    <p>
+      <template v-for="user in users">
+        <input
+          type="checkbox"
+          name=""
+          @click="userName = user.firstName"
+          :checked="userName === user.firstName"
+        />{{ user.firstName }}
+      </template>
+    </p>
+
+    <p>
+      <select name="" v-model="userName">
+        <option v-for="user in users" :value="user.firstName">
+          {{ user.firstName }}
+        </option>
+      </select>
+    </p>
+  </div>
 </template>
 
 <script>
-export default{
-    name: "FormView",
-}
+export default {
+  name: "FormView",
+
+  data() {
+    return {
+      userName: "Julio Marques",
+
+      users: [
+        { id: 1, firstName: "Julio Cesar Marques" },
+        { id: 2, firstName: "Ingrid Albuquerque" },
+        { id: 3, firstName: "Otavio Marques" },
+      ],
+    };
+  },
+};
 </script>
