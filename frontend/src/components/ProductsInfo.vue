00a33ba0442940449436e5d1f9e94ae394410f03
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-11 08:17:33 -0300

    Passagem de parametros options api

---
+++ frontend/src/components/ProductsInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Likes Recebido: {{ likes }}</p>
+    <p>Comentarios: {{ comments }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    likes: Number,
+    comments: Number
+  },
+  name: "ProductsInfo"
+};
+</script>
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,7 +1,29 @@
 <template>
+  <div>
     <h1>About Page</h1>
+    <ProductsInfo :likes="products.likes" v-bind:comments="products.comments" />
+  </div>
 </template>
 
 <style>
-
 </style>
+
+<script>
+import ProductsInfo from "@/components/ProductsInfo.vue";
+
+export default {
+  components: {
+    ProductsInfo,
+  },
+
+  data() {
+    return {
+      products: {
+        likes: 10,
+        comments: 40,
+      },
+    };
+  },
+  name: "AboutView.vue",
+};
+</script>
