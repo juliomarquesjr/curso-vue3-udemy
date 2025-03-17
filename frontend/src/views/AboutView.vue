38506f2310b4d895cf3a4855c73b963bf316d339
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-17 10:03:23 -0300

    Template Ref

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -2,6 +2,20 @@
   <div>
     <h1>About Page</h1>
     <ProductsInfo :likes="products.likes" v-bind:comments="products.comments" />
+
+    <h2>Imagem</h2>
+    <img
+      src="https://picsum.photos/200"
+      id="img"
+      ref="imgref"
+      alt="Imagem randomica"
+    />
+
+    <ul>
+      <li v-for="(item, index) in itens" :key="index" id="item" ref="itemref">
+        {{ item.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,6 +26,7 @@
 import ProductsInfo from "@/components/ProductsInfo.vue";
 
 export default {
+  name: "AboutView.vue",
   components: {
     ProductsInfo,
   },
@@ -22,8 +37,17 @@ export default {
         likes: 10,
         comments: 40,
       },
+      itens: [{ name: "Item 1" }, { name: "Item 2" }, { name: "Item 3" }],
     };
   },
-  name: "AboutView.vue",
+
+  mounted() {
+    // console.log(document.querySelector("#img"));
+    // console.log(document.querySelectorAll("#item"));
+    console.log(this.$refs.imgref["alt"]);
+    console.log(this.$refs.imgref["src"]);
+    // console.log(this.$refs.itemref);
+    // console.log(this.$refs.itemref);
+  },
 };
 </script>
