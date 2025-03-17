fe51568a2a92f3e04e8ce3ab7e1d406eea199d24
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-17 11:03:38 -0300

    Lodash com debounce usando a Composition API

--- frontend/src/views/SlotsView.vue
+++ frontend/src/views/SlotsView.vue
@@ -1,27 +1,37 @@
 <template>
-    <div>
-        <h1>Slotes</h1>
-        <button v-on:click="() => openModal = !openModal">Abrir Modal</button>
-        <button @click="() => openModal = !openModal">Abrir Modal</button>
+  <div>
+    <h1>Slotes</h1>
+    <button v-on:click="() => (openModal = !openModal)">Abrir Modal</button>
+    <button @click="() => (openModal = !openModal)">Abrir Modal</button>
 
-        <Modal v-if="openModal">
-            <template v-slot:header>
-                <h2>Header do Modal</h2>
-            </template>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti perspiciatis itaque cum magnam
-                necessitatibus quae corporis, expedita molestiae placeat natus totam? Tempora soluta neque dolorem
-                quibusdam odit voluptatibus nulla reprehenderit?</p>
-            <template v-slot:footer>
-                <h3>Footer do modal</h3>
-            </template>
-        </Modal>
+    <Modal v-if="openModal">
+      <template v-slot:header>
+        <h2>Header do Modal</h2>
+      </template>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti
+        perspiciatis itaque cum magnam necessitatibus quae corporis, expedita
+        molestiae placeat natus totam? Tempora soluta neque dolorem quibusdam
+        odit voluptatibus nulla reprehenderit?
+      </p>
+      <template v-slot:footer>
+        <h3>Footer do modal</h3>
+      </template>
+    </Modal>
 
-    </div>
+    <h2>Lodash</h2>
+    <input placeholder="Pesquisar" @keyup="search" />
+  </div>
 </template>
 
 <script setup>
-import Modal from '@/components/Modal.vue';
-import { ref } from 'vue';
+import _ from "lodash";
+import Modal from "@/components/Modal.vue";
+import { ref } from "vue";
 
-const openModal = ref(false)
+const openModal = ref(false);
+
+const search = _.debounce(() => {
+  console.log("Esperei e executei");
+}, 1000);
 </script>
