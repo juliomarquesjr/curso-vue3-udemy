123a41561e8414cd87a85ce0278eab4739b956c5
Julio Marques <juliomarquesjr@yahoo.com.br>
2025-03-17 10:44:36 -0300

    Lodash Debounce com Options API

--- frontend/src/views/SlotsView.vue
+++ frontend/src/views/SlotsView.vue
@@ -3,23 +3,27 @@
     <h1>Slotes Option API</h1>
     <button @click="toggleModal">Abrir / Fechar</button>
     <Modal v-if="openModal">
-        <template v-slot:header>
-            <h2>Titulo do modal</h2>
-        </template>
-        
-        <template v-slot:default>
-            <h3>Conteudo do Slote sendo enviado!!</h3>
-        </template>
+      <template v-slot:header>
+        <h2>Titulo do modal</h2>
+      </template>
 
-        <template #footer>
-            <p>Texto do modal aqui para o rodape</p>
-        </template>
+      <template v-slot:default>
+        <h3>Conteudo do Slote sendo enviado!!</h3>
+      </template>
+
+      <template #footer>
+        <p>Texto do modal aqui para o rodape</p>
+      </template>
     </Modal>
+
+    <h2>Lodash</h2>
+    <input @keyup="search" placeholder="Pesquisar" />
   </div>
 </template>
 
 <script>
 import Modal from "@/components/Modal.vue";
+import _ from "lodash"; //Importando a lib lodash
 
 export default {
   data() {
@@ -35,6 +39,11 @@ export default {
     toggleModal() {
       this.openModal = !this.openModal;
     },
+
+    //O debounce foi usado para somente executar a função apos alguns segundos, no caso 1 segundo
+    search: _.debounce(() => {
+      console.log("Aguardei e apertei");
+    }, 1000),
   },
 };
 </script>
